a61623b11dd687ebaff4ed2a51f80cf84f5df42e
Gwang-Jin Kim <gwang.jin.kim.phd@gmail.com>
2021-05-21 13:01:14 +0200

    add case all_numeric=True

--- numpickle/__init__.py
+++ numpickle/__init__.py
@@ -1,4 +1,4 @@
 __version__ = '0.1.0'
 
-from .main import save_numpickle, load_numpickle
+from .main import save_numpickle, load_numpickle, save_file_as_numpickle
 
--- numpickle/main.py
+++ numpickle/main.py
@@ -1,17 +1,29 @@
 import pandas as pd
 import numpy as np
 import pickle
+import os
 
-def save_numpickle(df, outfpath):
+
+def save_numpickle(df, outfpath, all_numeric=False):
     arr, colnames, rownames = df.to_numpy(), df.columns, df.index
     np.save(arr=arr, file=outfpath)
-    pickle.dump({'colnames': colnames, 'rownames': rownames, 'dtypes': df.dtypes}, 
+    pickle.dump({'colnames': colnames, 'rownames': rownames, 'dtypes': None if all_numeric else df.dtypes}, 
                 open(outfpath + ".pckl", "wb"))
 
 def load_numpickle(fpath):
     df = pd.DataFrame(np.load(fpath, allow_pickle=True))
     with open(fpath + ".pckl", "rb") as fin:
         meta = pickle.load(fin)
-    df.index, df.columns, dtypes = meta['rownames'], meta['colnames'], meta['dtypes']
-    df = df.astype(dtypes)
+    # if no 'types' present, assuming all_numeric
+    df.index, df.columns, dtypes = meta['rownames'], meta['colnames'], meta.get('dtypes', None)
+    if dtypes is not None:
+        df = df.astype(dtypes)
     return df
+
+def save_file_as_numpickle(fpath, sep="\t", all_numeric=False, deletep=False, *args, **kwargs):
+    df = pd.read_csv(fpath, sep=sep, *args, **kwargs)
+    save_numpickle(df, fpath.replace(".tab", ".npy"), all_numeric=all_numeric)
+    if deletep:
+        os.remove(fpath)
+
+
